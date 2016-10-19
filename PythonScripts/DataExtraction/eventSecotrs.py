858cefac31f5f75b7df184e121f70047ce43cc09
Sam Dixon <samdixon05@gmail.com>
2016-10-19 18:10:20 +0100

    Refactored event extraction into a single class

--- PythonScripts/DataExtraction/eventSecotrs.py
+++ PythonScripts/DataExtraction/eventSecotrs.py
@@ -1,29 +1,29 @@
 import pymysql
 
 
-def geteventsectors(event_table, pattern_no, tollearnace):
+def get_event_sectors(event_table, pattern_no):
 
+    tolerance = 10
     con = pymysql.connect(host='localhost', port=3306, user='root', passwd='admin', db='schoolsv6')
     curr = con.cursor()
 
-    pattern_sectors = []
-
     curr.execute("SELECT patternX FROM fittssectorid WHERE patternRef = %s", pattern_no)
-
+    pattern_sectors = []
     for row in curr:
         pattern_sectors.append(list(row))
 
-    for val in pattern_sectors:
-        print(val)
-
-    header = ['collectionref', 'sequenceNo', 'patternRef', 'xCoord', 'sectorID']
-    event_sectors = [header]
-
-
     query = "SELECT collectionref, sequenceNo, patternRef, xCoord FROM %s WHERE patternRef = %s" %(event_table, pattern_no)
     curr.execute(query)
+    header = ['collectionref', 'sequenceNo', 'patternRef', 'xCoord', 'sectorID']
+    event_sectors = [header]
     for row in curr:
-        print(row)
+        sector_count = 1
+        for val in pattern_sectors:
+            if row[3] > (val[0] - tolerance):
+                sector_count += 1
 
-    return
+        temp_row = list(row)
+        temp_row.append(sector_count)
+        event_sectors.append(temp_row)
 
+    return event_sectors
--- PythonScripts/DataExtraction/main.py
+++ PythonScripts/DataExtraction/main.py
@@ -1,5 +1,8 @@
-from DataExtraction.eventSecotrs import geteventsectors
+from DataExtraction.eventSecotrs import get_event_sectors
 
 table = "fittslooplocations"
 
-geteventsectors(table, 3, 3)
+loop_sectors = get_event_sectors(table, 3)
+for val in loop_sectors:
+    print(val)
+

679b9e4197c192deee8f5f64e29b57f2a43ff7ae
Sam Dixon <samdixon05@gmail.com>
2016-10-19 22:55:09 +0100

    Can now produce a list of invalid sectors for each collection.

--- PythonScripts/DataExtraction/event_sectors.py
+++ PythonScripts/DataExtraction/event_sectors.py
@@ -6,16 +6,12 @@ curr = con.cursor()
 
 def get_event_sectors(event_table, pattern_no):
     tolerance = 10
-
+    event_sectors = []
     pattern_sectors = get_pattern_sectors(pattern_no)
 
-    query = "SELECT collectionref, sequenceNo, patternRef, xCoord FROM %s WHERE patternRef = %s" % (
-        event_table, pattern_no)
+    query = "SELECT collectionref, sequenceNo, patternRef, xCoord FROM %s WHERE patternRef = %s" \
+            % (event_table, pattern_no)
     curr.execute(query)
-
-    event_sectors = [[event_table, pattern_no]]
-    header = ['collectionref', 'sequenceNo', 'patternRef', 'xCoord', 'sectorID']
-    event_sectors.append(header)
 
     for row in curr:
         sector_count = 1
@@ -30,9 +26,26 @@ def get_event_sectors(event_table, pattern_no):
     return event_sectors
 
 
+def get_collection_data(pattern):
+    collection_data = []
+    curr.execute("SELECT idCollection, sequenceNo, patternref, agegroupref FROM fittssectortimes WHERE patternref = %s", pattern)
+
+    for row in curr :
+        collection_data.append(list(row))
+
+    return collection_data
+
+
 def get_pattern_sectors(pattern_no):
     curr.execute("SELECT patternX FROM fittssectorid WHERE patternRef = %s", pattern_no)
     pattern_sectors = []
     for row in curr:
         pattern_sectors.append(list(row))
     return pattern_sectors
+
+
+def close_connection():
+    curr.close()
+    con.close()
+    print("Database connection closed")
+    return
--- PythonScripts/DataExtraction/main.py
+++ PythonScripts/DataExtraction/main.py
@@ -1,13 +1,27 @@
-from DataExtraction.event_sectors import get_event_sectors
+from DataExtraction.event_sectors import *
 
 tables = ["fittslooplocations", "fittsstasislocations", "fittsliftlocations"]
-patterns = [3, 4]
-event_sectors = []
-for table in tables:
-    for pattern in patterns:
-       event_sectors.append(get_event_sectors(table, pattern))
+event_columns = ['collectionref', 'sequenceNo', 'patternRef', 'xCoord', 'sectorID']
+collection_columns = ['idCollection', 'sequenceNo', 'patternref', 'agegroupref']
+pattern_3_event_sectors = []
+pattern_4_event_sectors = []
 
-for val in event_sectors:
-    print(val[0])
-    print(val[1])
-    print(val[2])
+for table in tables:
+    pattern_3_event_sectors.append(get_event_sectors(table, 3))
+for table in tables:
+    pattern_4_event_sectors.append(get_event_sectors(table, 4))
+
+pattern_3_collection_data = get_collection_data(3)
+pattern_4_collection_data = get_collection_data(4)
+
+for collRow in pattern_3_collection_data:
+    for collection in pattern_3_event_sectors:
+        for event in collection:
+            if event[0] == collRow[0] and event[1] == collRow[1] and event[2] == collRow[2]:
+                collRow.append(event[4])
+
+for val in pattern_3_collection_data:
+    print(val)
+
+
+close_connection()

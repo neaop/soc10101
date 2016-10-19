f1ed5247002145deb69f42c7bf67bf54e088d7dd
Sam Dixon <samdixon05@gmail.com>
2016-10-19 20:47:38 +0100

    Finished generic extraction class.

--- PythonScripts/DataExtraction/main.py
+++ PythonScripts/DataExtraction/main.py
@@ -1,8 +1,12 @@
-from DataExtraction.eventSecotrs import get_event_sectors
+from DataExtraction.event_sectors import get_event_sectors
 
-table = "fittslooplocations"
+tables = ["fittslooplocations", "fittsstasislocations", "fittsliftlocations"]
+patterns = [3, 4]
+event_sectors = []
+for table in tables:
+    for pattern in patterns:
+       event_sectors.append(get_event_sectors(table, pattern))
 
-loop_sectors = get_event_sectors(table, 3)
-for val in loop_sectors:
-    print(val)
-
+for val in event_sectors:
+    print(val[0])
+    print(val[2])
